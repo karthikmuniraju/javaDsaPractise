cf49f45f469f6a5771edb9c39e97f1358c775574
Karthik M <karthik.m@Karthiks-MacBook-Air.local>
2025-07-05 10:51:14 +0530

    added day10 problems with solution one leetcode code only

---
+++ DSAPractiseFromDay4/src/leetcodeDay10.java
@@ -0,0 +1,17 @@
+public class leetcodeDay10 {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int [] result = new int[n];
+        result[0]=1;
+        for(int i=1;i<n;i++){
+            result[i]=nums[i-1]*result[i-1];
+        }
+        int suffixProduct=1;
+        for(int i=n-1;i>=0;i--){
+            result[i]*=suffixProduct;
+            suffixProduct*=nums[i];
+        }
+        return result;
+
+    }
+}
